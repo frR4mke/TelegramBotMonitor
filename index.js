60ca4e679812399cc7fe1008c1355ceab9b12fca
frR4mke <44607143+frR4mke@users.noreply.github.com>
2021-10-05 12:13:31 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const functions = require("firebase-functions");
+
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const telegraf_1 = require("telegraf");
+const pleerParser = __importStar(require("../functions/pleerparser/pleerparser"));
+const bot = new telegraf_1.Telegraf('2034784132:AAGMPPO1rciFpIZC9dfU8c_npMTblDIGDhs');
+bot.command('price', (ctx) => {
+    pleerParser.getPrices().catch(e => {
+        console.log(e);
+    }).then(message => {
+        if (message) {
+            ctx.reply(message);
+        }
+    });
+});
+bot.help((ctx) => ctx.reply('Показываю актуальные цены с сайта https://www.pleer.ru/ \nКомманды: \n /price - цены на iphone 12'));
+bot.on('text', ctx => ctx.reply('Для вывода обрабатываемых команд, введите /help'));
+bot.launch();
+function getCommandArg(text, command) {
+    return text.split(command)[1].replace(/\s/g, "");
+}
+//# sourceMappingURL=bot.js.map
---
+++ pleerparser/pleerparser.js
@@ -0,0 +1,59 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getPrices = void 0;
+const rest_1 = require("../rest/rest");
+const typescript_collections_1 = require("typescript-collections");
+async function getPrices() {
+    return (0, rest_1.readBodyFrom)(`https://www.pleer.ru/search_iphone+12_1-61,2-0,7-(88),3-(),4-(),5-0,8-0,9-0,6-7.html`)
+        .catch(e => {
+        console.log(e);
+    }).then(response => {
+        const root = response;
+        if (root) {
+            const _item_names = root.querySelectorAll('.pad_r');
+            const _urls = _item_names.map(x => x.querySelector('a')).map(x => x.rawAttributes);
+            const _prices = root.querySelectorAll('.product_price_color1');
+            const item_names = _item_names.map(x => x.structuredText);
+            const prices = _prices.map(x => x.structuredText);
+            const r = new typescript_collections_1.Dictionary();
+            const not_available = 'Нет в наличии';
+            for (let index = 0; index < item_names.length; index++) {
+                const name = item_names[index];
+                const price = prices[index];
+                const url = _urls[index].href;
+                const _m = {
+                    price: price !== null && price !== void 0 ? price : not_available,
+                    url: url
+                };
+                r.setValue(name, _m);
+            }
+            let message = '';
+            for (let index = 0; index < r.keys().length; index++) {
+                const key = r.keys()[index];
+                let value = r.getValue(key);
+                if (value === null || value === void 0 ? void 0 : value.price) {
+                    if (value.price != not_available) {
+                        value.price = value.price.substring(0, value.price.indexOf('руб.'));
+                    }
+                }
+                if (value === null || value === void 0 ? void 0 : value.url) {
+                    value.url = 'https://www.pleer.ru/' + value.url;
+                }
+                message +=
+                    'НАИМЕНОВАНИЕ: ' + key +
+                        '\n' +
+                        'ССЫЛКА ' + (value === null || value === void 0 ? void 0 : value.url) +
+                        '\n' +
+                        'ЦЕНА: ' + (value === null || value === void 0 ? void 0 : value.price);
+                //     'ЦЕНА: ' + value?.price; +
+                //     '\n' +
+                //    ;
+                message += '\n';
+                message += '\n';
+            }
+            return message;
+        }
+    });
+}
+exports.getPrices = getPrices;
+//# sourceMappingURL=pleerparser.js.map
